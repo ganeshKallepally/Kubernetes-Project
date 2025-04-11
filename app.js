50d8bcc1a3f916d4c53a05fe5a13b229e296c364
ganeshkallepally127@gmail.com <root@ip-172-31-0-7.ap-southeast-2.compute.internal>
2025-04-11 09:03:35 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js App running in Docker!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
